017c2e4cfc577f5ffe7f0f184c8667f3cb9964f1
Mark Clu Heramis <chumheramis@gmail.com>
2015-12-06 01:24:20 +0800

    Part 1 improved
    Added description and codes of part 1

--- src/part_1/main.java
+++ src/part_1/main.java
@@ -1,7 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Welcome to Java Tutorials
+ In this part of the tutorial I am going to introduce you to the basic composition of a
+ Java program. Below you'll see a basic program that does nothing.
+
+ At line [#] is the package which is responsible for the file system structure of
+ your java program, but don't worry if you do not understand it, it's ok to not
+ even put it there.
+
+ Starting form line [#] to [#] is a comment, java ignores all the comments and
+ is used only for people (For documenting codes). You'll know it soon enough so
+ don't be afraid if you are not catching up with me.
+
+ At line [#] is the class declaration.
+ The syntax of declaring class is as follows
+[visibility] class [class/file name]
+In this case, our filename is main.java there for our class name should be main.
  */
 package part_1;
 
@@ -10,5 +23,5 @@ package part_1;
  * @author systemzero
  */
 public class main {
-    
+
 }
