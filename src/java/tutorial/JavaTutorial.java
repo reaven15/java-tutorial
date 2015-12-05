d497f9bb1e39eb1bb794fceb8d64e2e364dc1f03
Mark Clu Heramis <chumheramis@gmail.com>
2015-12-05 15:06:47 +0800

    Initialization of project content management

---
+++ src/java/tutorial/JavaTutorial.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.tutorial;
+
+/**
+ *
+ * @author systemzero
+ */
+public class JavaTutorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
